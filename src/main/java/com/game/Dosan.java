1115fcec8ae6d63ee3eb64302fe1d2aecc2017b8
Mateus Dosan <mateusdosan07@gmail.com>
2026-05-23 18:37:00 -0300

    Add personagem Dosan

--- src/main/java/com/game/Dosan.java
+++ src/main/java/com/game/Dosan.java
@@ -1,6 +1,6 @@
 package com.game;
 
-public class Dosan extends PersonagemBase{
+public class Dosan extends PersonagemBase {
 
     public Dosan(String nome, int numeroCamisa, double velocidadeMovimento, String caminhoSprite) {
         super("Dosan", 67, 5.0, "/com/game/sprites/gk.png");
@@ -8,6 +8,6 @@ public class Dosan extends PersonagemBase{
 
     @Override
     public void Poder() {
-        System.out.println("Soltando Fumaça da confusão");
+        System.out.println("Soltando a fumaça da confusão");
     }
 }
